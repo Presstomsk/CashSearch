3930675ff1c435fe7f4b69d26804c41af5880a6e
Roman <info@presstomsk.ru>
2021-09-11 16:21:33 +0700

    Поиск Кэш

--- CashSearch/CashSearch.cs
+++ CashSearch/CashSearch.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace CashSearch
 {
-    class CashSearch
-    {
+    public static class CashSearch
+    {        
+        private static string _yandex = @"C:\Users\Admin\AppData\Local\Yandex\YandexBrowser\User Data\Default\Cache";
+        private static string _googleChrome = @"C:\Users\Admin\AppData\Local\Google\Chrome\User Data\Default\Cache";
+        private static string _opera = @"C:\Users\Admin\AppData\Local\Opera Software\Opera Stable\Cache";
+
+        public static bool CashSearching(string path)
+        {
+            return (Directory.Exists(path)) ;                           
+        }
+
+        public static void YandexCashSearch()
+        {
+            if (CashSearching(_yandex)) Console.WriteLine("Обнаружен Кэш Yandex");
+        }
+        public static void ChromeCashSearch()
+        {
+            if (CashSearching(_googleChrome)) Console.WriteLine("Обнаружен Кэш Google Chrome");
+        }
+        public static void OperaCashSearch()
+        {
+            if (CashSearching(_opera)) Console.WriteLine("Обнаружен Кэш Opera");
+        }
+
     }
 }
--- CashSearch/Program.cs
+++ CashSearch/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.IO;
 
 namespace CashSearch
 {
     class Program
     {
         static void Main()
-        {
-            
+        {            
+            CashSearch.YandexCashSearch();
+            CashSearch.ChromeCashSearch();
+            CashSearch.OperaCashSearch();
         }
     }
 }
