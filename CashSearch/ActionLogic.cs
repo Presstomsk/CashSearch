97334a2020a7fe25501773532469e6d4836e9fbd
Roman <info@presstomsk.ru>
2021-09-17 21:01:04 +0700

    FINAL VERSION

--- CashSearch/ActionLogic.cs
+++ CashSearch/ActionLogic.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace CashSearch
 {
-    public delegate void Operations(string address, DataBase db);
+    public delegate void Operations(string address);
     
     public static class ActionLogic
     {
-        public static bool Actions(Dictionary<string,Operations> _menu, string key, string address, bool flag, DataBase db)
+        public static bool Actions(Dictionary<string,Operations> _menu, string key)
         {
-            if (key == "0") return ExitProgram();            
-            if (!flag) return FalseChoice();
-            return TrueChoice(_menu,key,address,db);
+            if (!_menu.ContainsKey(key)) return FalseChoice();
+            if (key == "0") return ExitProgram();         
+            if (!Program._flags[key]) return FalseChoice();
+            return TrueChoice(_menu,key,Program._cacheAddresses[key]);
         }
         public static bool ExitProgram() { return true; }
         public static bool FalseChoice()
@@ -22,9 +23,9 @@ namespace CashSearch
             Show.ErrorChoiсe();
             return false;
         }
-        public static bool TrueChoice(Dictionary<string, Operations> _menu, string key, string address, DataBase db)
+        public static bool TrueChoice(Dictionary<string, Operations> _menu, string key, string address)
         {
-            _menu[key](address, db);
+            _menu[key](address);
             return true;
         }
 
--- CashSearch/Program.cs
+++ CashSearch/Program.cs
@@ -14,76 +14,85 @@ namespace CashSearch
 
         static void Main()
         {
-            DataBase db = new DataBase();
-            _cacheAddresses =new Dictionary<string, string>
+            bool flagAll = false;
+            _cacheAddresses =new Dictionary<string, string> //Словарь адресов папок кэша
             {
+                {"0",null},
                 {"1", CashSearch.adr[0]},
                 {"2", CashSearch.adr[1]},
-                {"3", CashSearch.adr[2]}
+                {"3", CashSearch.adr[2]},
+                {"4", null}
             };
 
-            _flags = new Dictionary<string, bool>
+            _flags = new Dictionary<string, bool> // Проверка наличия папок кэш на диске 
                {
+                {"0", false },
                 {"1", CashSearch.CashSearching(_cacheAddresses["1"])},
                 {"2", CashSearch.CashSearching(_cacheAddresses["2"])},
-                {"3", CashSearch.CashSearching(_cacheAddresses["3"])}                
+                {"3", CashSearch.CashSearching(_cacheAddresses["3"])},
+                {"4", flagAll}
                };
-            _menu = new Dictionary<string, Operations>
+            _menu = new Dictionary<string, Operations> //Словарь Меню
                {
+                {"0", LoggingAndClearing},
                 {"1", LoggingAndClearing},
                 {"2", LoggingAndClearing},
-                {"3", LoggingAndClearing},                
-               };        
+                {"3", LoggingAndClearing},
+                {"4", AllLoggingAndClearing}
+               };
+                        
+            // Меню
 
             Message message;
             message = Show.StartString;
             if (_flags["1"]) message += Show.GoogleChromeInfo;
             if (_flags["2"]) message += Show.OperaInfo;
             if (_flags["3"]) message += Show.YandexInfo;
-            if ((Convert.ToInt32(_flags["1"]) + Convert.ToInt32(_flags["2"]) + Convert.ToInt32(_flags["3"])) >1) message += Show.AllInfo;
+            if ((Convert.ToInt32(_flags["1"]) + Convert.ToInt32(_flags["2"]) + Convert.ToInt32(_flags["3"])) > 1)
+            {
+                message += Show.AllInfo;
+                _flags["4"] = true;
+
+            }
             if (message != Show.StartString) message += Show.EndInfo;
-            else message = Show.NonResult;      
-            message();
+            else
+            {
+                message = Show.NonResult;
+                message();
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
 
             
             //Вывод на экран удаленных файлов, запись лога в БД, удаление файлов
-            if (_flags["1"] || _flags["2"] || _flags["3"])
-            {
-                
+                          
                 bool flag=true;
                 do
                 {
-                    var key = Console.ReadLine();
-                    
-                    if (_menu.ContainsKey(key)) flag = ActionLogic.Actions(_menu, key, _cacheAddresses[key],_flags[key],db);
-                    else  flag=ActionLogic.FalseChoice();
-                    /* else
-                     {
-                         if ((Convert.ToInt32(googleCFlag) + Convert.ToInt32(operaCFlag) + Convert.ToInt32(yandexCFlag)) <= 1)
-                         {
-                             ActionLogic.FalseChoice();
-                         }
-                         else
-                         {
-                             if (googleCFlag) flag = Actions("1", db);
-                             if (operaCFlag) flag = Actions("2", db);
-                             if (yandexCFlag) flag = Actions("3", db);
-                         }
-                     }*/
-
+                    var key = Console.ReadLine();                    
+                    flag = ActionLogic.Actions(_menu, key);
+               
                 } while (!flag);
                 
                 
-            }        
+                   
         
         }                    
-        public static void LoggingAndClearing(string address, DataBase db)
+        public static void LoggingAndClearing(string address) 
         {
-            Console.WriteLine(address);
+            DataBase db = new DataBase();
             string[] files = CashSearch.CashFiles(address);
+            Console.WriteLine("УДАЛЕНИЕ ФАЙЛОВ");
             CashSearch.ShowFiles(files);
             db.ExportToDB(files);
             CashSearch.CashDelete(address);           
+        }
+        public static void AllLoggingAndClearing(string address)
+        {
+            if (_flags["1"]) ActionLogic.Actions(_menu, "1");
+            if (_flags["2"]) ActionLogic.Actions(_menu, "2");
+            if (_flags["3"]) ActionLogic.Actions(_menu, "3");
+
         }
     }
 }
